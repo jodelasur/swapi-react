ba47255f08d7c6af718dc250aaadc3cd7aefed78
Jodel Asur <jodelasur@gmail.com>
2019-03-22 22:31:24 +0800

    Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
 import rootSaga from "../sagas";
 
+const composeEnhancers = composeWithDevTools({trace: true, traceLimit: 25});
 const saga = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
     undefined,
-    applyMiddleware(saga)
+    composeEnhancers(applyMiddleware(saga))
 );
 
 saga.run(rootSaga);
