1d96408e8dc595b73b5e4016268e8cef6b5ee8be
Jodel Asur <jodelasur@gmail.com>
2019-03-22 23:29:43 +0800

    Set max width for App and center it

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import styled from "styled-components";
 import Search from "./Search";
 
 const Div = styled.div`
-  margin: 20px;
+  margin: 20px auto;
+  max-width: 1140px;
 `;
 
 const App = () =>
